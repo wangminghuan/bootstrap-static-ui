9e50cd7c5170d7103a34f6787a09c711772ccdd4
dino <853145189@qq.com>
2019-04-09 19:10:46 +0800

    add 20190409

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,16 @@
 const router = require('koa-router')()
-
 router.get('/', async (ctx, next) => {
+  await ctx.render('login', {
+    title: 'Hello Koa 2!'
+  })
+})
+router.get('/index', async (ctx, next) => {
   await ctx.render('index', {
+    title: 'Hello Koa 2!'
+  })
+})
+router.get('/form/index', async (ctx, next) => {
+  await ctx.render('/form/index', {
     title: 'Hello Koa 2!'
   })
 })
@@ -16,9 +25,18 @@ router.get('/table/data/index', async (ctx, next) => {
   })
 })
 router.get('/table/basic/index', async (ctx, next) => {
-  await ctx.render('/table/data/index', {
+  await ctx.render('/table/basic/index', {
     title: 'Hello Koa 2!'
   })
 })
-
+router.get('/chart/index', async (ctx, next) => {
+  await ctx.render('/chart/index', {
+    title: 'Hello Koa 2!'
+  })
+})
+router.get('/login', async (ctx, next) => {
+  await ctx.render('/login.json', {
+    title: 'Hello Koa 2!'
+  })
+})
 module.exports = router
